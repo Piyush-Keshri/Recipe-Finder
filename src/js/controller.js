2697ab5264e1f1bc161c7aca0e477e3e87d5011b
Piyush Keshri <piyushkeshri383@gmail.com>
2022-02-24 12:39:50 +0530

    commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,15 @@
 import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import { async } from 'regenerator-runtime';
+
+if(module.hot){
+  module.hot.accept();
+}
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -31,6 +36,8 @@ const controlRecipes = async function () {
 const controlSearchResults = async function(){
 
   try{
+    console.log(resultsView); 
+    
     // 1) Get search Query
     const query = searchView.getQuery();
     if(!query) return;
@@ -39,7 +46,7 @@ const controlSearchResults = async function(){
    await model.loadSearchResults(query);
 
   //  3) Render Search Results
-   console.log(model.state.search.results);
+   resultsView.render(model.state.search.results);
   }
   catch(err){
     console.log(err);
