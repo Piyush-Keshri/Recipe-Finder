b80cc7db8bbcdc9f76328b34abe855515dfab1f8
Piyush Keshri <piyushkeshri383@gmail.com>
2022-02-25 20:36:36 +0530

    commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,6 +2,7 @@ import * as model from './model.js'
 import recipeView from './views/recipeView.js'
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -45,17 +46,29 @@ const controlSearchResults = async function(){
     // 2) Load search results
    await model.loadSearchResults(query);
 
-  //  3) Render Search Results
+    //  3) Render Search Results
    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render Initial Pagination Buttons
+    paginationView.render(model.state.search);
+
   }
   catch(err){
     console.log(err);
   }
 };
-controlSearchResults();
+// controlSearchResults();
+const controlPagination = function(goToPage){
+  //  1) Render New Search Results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) Render New Initial Pagination Buttons
+  paginationView.render(model.state.search);
+}
 
 const init = function(){
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 }
 init();
